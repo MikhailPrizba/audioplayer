4e53d5b6750d384df32238dddc333e83729de9c7
mikhail <mishaprizby@gmail.com>
2023-08-06 13:21:49 +0300

    add encoding

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-PLAYLIST_FOLDER = '/home/mikhail/Рабочий стол/music'
-ANNOUNCEMENT_SONG_NUMBER = 2
-TEST_MODE = 1
+ANNOUNCEMENT_SONG_NUMBER = 4
 MESSAGE_FOLDER = '/home/mikhail/Рабочий стол/music'
+TEST_MODE = 1
+PLAYLIST_FOLDER = '/home/mikhail/Рабочий стол/music'
